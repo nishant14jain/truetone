a7a869370430712fabe95ea45cee76ccba7f5f3d
nishantjain1407 <nishantjain1407@gmail.com>
2017-07-04 00:52:49 +0530

    test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Truetone Inc</title>
+
+  <!-- Fonts -->
+  <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+
+  <!-- Styles -->
+</head>
+<body>
+  <div class='headnav'>
+    <div class='limitWidth'>
+      <div class='pull-left'>
+        <ul class='headnav-links'>
+          <li>
+            <a href='#!'>Dyes</a>
+          </li>
+          <li>
+            <a href='#!'>Yarns</a>
+          </li>
+          <li>
+            <a href='#!'>Fabrics</a>
+          </li>
+        </ul>
+      </div>
+      <div class='logo'>
+        <img src='{{asset('images/logo.svg')}}'>
+      </div>
+      <div class='pull-right'>
+        <ul class='headnav-links'>
+          <li>
+            <a href='#!'>About Us</a>
+          </li>
+          <li>
+            <a href='#!'>R & D</a>
+          </li>
+          <li>
+            <a href='#!'>Contact</a>
+          </li>
+          <li>
+            <a href='#!'>Account</a>
+          </li>
+        </ul>
+      </div>
+      <div class='clearfix'></div>
+    </div>
+  </div>
+
+  <div class='page-content homepage'>
+    <section class='landing'>
+      <img src="{{asset('images/homepage/Truetone_Home.png')}}">
+    </section>
+    <section class='featuring'>
+      <div class="strip dye" style='background:url({{asset('images/homepage/dye.png')}})'>
+        <div class='overlay'>
+          <h1>Dyes</h1>
+          <p>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</p>
+        </div>
+      </div>
+      <div class="strip yarn" style='background:url({{asset('images/homepage/yarn.png')}})'>
+        <div class='overlay'>
+          <h1>Yarns</h1>
+          <p>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</p>
+        </div>
+      </div>
+      <div class="strip fabric" style='background:url({{asset('images/homepage/fabric.png')}})'>
+        <div class='overlay'>
+          <h1>Fabrics</h1>
+          <p>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</p>
+        </div>
+      </div>
+      <div class='clearfix'></div>
+    </section>
+    <section class='whoweare'>
+      <div class='container'>
+        <h1>Who we are</h1>
+        <h3>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</h3>
+      </div>
+    </section>
+    <section class='ourprocess'>
+      <div class='container'>
+        <h1>Our process</h1>
+        <h3>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</h3>
+      </div>
+    </section>
+  </div>
+
+  <div class='footer'>
+
+  </div>
+</body>
+</html>
