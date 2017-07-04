868f054a7b52fb51246016b00f284d0a8555a82d
nishant14jain <nishantjain1407@gmail.com>
2017-07-04 12:51:09 +0530

    certified section footer social icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,10 @@
   <title>Truetone Web</title>
 
   <!-- Fonts -->
-  <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 
   <!-- Styles -->
+  <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
   <div class='headnav'>
@@ -78,20 +79,53 @@
     </section>
     <section class='whoweare'>
       <div class='container'>
-        <h1>Who we are</h1>
+        <h1>Who We Are</h1>
+        <hr/>
         <h3>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</h3>
       </div>
     </section>
     <section class='ourprocess'>
       <div class='container'>
-        <h1>Our process</h1>
+        <h1>Our Process</h1>
+        <hr />
         <h3>Truetone is the leading online shop of herbal dyed organic clothes for men and women. We have a wide range of natural clothing from underwear to fabrics and towels and more. Our men's clothes cover organic cotton tee, boxer, brief, vest and more while womens clothing covers bikini, hipster, camisole, organic t-shirts.</h3>
+      </div>
+    </section>
+    <section class='certified'>
+      <div class='container'>
+        <h1>Be Truetone Certified</h1>
+        <hr />
+        <div class='row'>
+          <div class='col-md-6 col-lg-6 col-sm-6 col-xs-12'>
+            <div class='creative-wrapper'>
+              <img src='{{asset('images/homepage/certified.svg')}}' />
+            </div>
+          </div>
+          <div class='col-md-6 col-lg-6 col-sm-6 col-xs-12'>
+            <div class='desc-wrapper text-left'>
+              <h2>Apply for Certificate</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 
   <div class='footer'>
-
+    <div class='social-wrapper'>
+      <div class="social-btns">
+        <a class="btn facebook" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="btn twitter" href="#"><i class="fa fa-twitter"></i></a>
+        <a class="btn google" href="#"><i class="fa fa-google"></i></a>
+        <a class="btn dribbble" href="#"><i class="fa fa-dribbble"></i></a>
+        <a class="btn skype" href="#"><i class="fa fa-skype"></i></a>
+      </div>
+    </div>
   </div>
 </body>
 </html>
