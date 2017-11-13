a397b33797bda5de62a5302a353c9d9649259e5b
nishant14jain <nishantjain1407@gmail.com>
2017-11-13 16:08:48 +0530

    shade card popup

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -40,7 +40,7 @@ var shadeCardData = {
 	},
 	'haldi-yellow':{
 		ttx:'2-100 TTX',
-		herbs:['TUrmeric(Curcuma Longa)','Pomegranate(Punica Granatum)'],
+		herbs:['Turmeric(Curcuma Longa)','Pomegranate(Punica Granatum)'],
 		pantone:'12-0738 TPX',
 		name:'Haldi Yellow',
 		color:'#F1DA6D'
@@ -50,135 +50,152 @@ var shadeCardData = {
 		herbs:['Indigo(Indigofera)','Harda(Terminalia Chebula)'],
 		pantone:'15-0743 TPX',
 		name:'Pale Celery',
-		color:'#000'
+		color:'#C4A548'
 	},
 	'dahlia-green':{
 		ttx:'3-100 TTX',
 		herbs:['Iron Vat','Harda(Terminalia Chebula)'],
 		pantone:'13-0624 TPX',
 		name:'Dahlia Green',
-		color:'#000'
+		color:'#D6D08F'
 	},
 	'kasam-olive':{
 		ttx:'3-101 TTX',
 		herbs:['Iron Vat','Harda(Terminalia Chebula)'],
 		pantone:'16-0737 TPX',
 		name:'Kasam Olive',
-		color:'#000'
+		color:'#A79952'
 	},
 	'cherry-pink':{
 		ttx:'8-101 TTX',
 		herbs:['Burnt Ash','Madder(Rubia Cordifolia)','Sea Salt'],
 		pantone:'15-1821 TPX',
 		name:'Cherry Pink',
-		color:'#000'
+		color:'#F495A0'
 	},
 	'ash-pink':{
 		ttx:'8-102 TTX',
 		herbs:['Burnt Ash','Madder(Rubia Cordifolia)','Sea Salt'],
 		pantone:'14-1511 TPX',
 		name:'Ash Pink',
-		color:'#000'
+		color:'#EDB7B9'
 	},
 	'beetle-pink':{
 		ttx:'8-103 TTX',
 		herbs:['Burnt Ash','Madder(Rubia Cordifolia)','Sea Salt','Iron Earth'],
 		pantone:'14-1219 TPX',
 		name:'Beetle Pink',
-		color:'#000'
+		color:'#FBC0AB'
 	},
 	'okhra-orange':{
 		ttx:'4-101 TTX',
 		herbs:['Madder(Rubia Cordifolia)','Pomegranate(Punica Granatum)','Woodflower'],
 		pantone:'14-1041 TPX',
 		name:'Okhra Orange',
-		color:'#000'
+		color:'#DFA95B'
 	},
 	'pomo-orange':{
 		ttx:'4-102 TTX',
 		herbs:['Flame of Forest(Butea Monosperma)','Madder(Rubia Cordifolia)'],
 		pantone:'16-1337 TPX',
 		name:'Pomo Orange',
-		color:'#000'
+		color:'#D68158'
 	},
 	'anna-orange':{
 		ttx:'4-100 TTX',
 		herbs:['Flame of Forest(Butea Monosperma)','Madder(Rubia Cordifolia)'],
 		pantone:'15-1234 TPX',
 		name:'Anna Orange',
-		color:'#000'
+		color:'#DF9C66'
 	},
 	'brazil-brown':{
 		ttx:'7-100 TTX',
 		herbs:['Madder(Rubia Cordifolia)','Woodflower','Iron Earth','Sea Salt'],
 		pantone:'17-1322 TPX',
 		name:'Brazil Brown',
-		color:'#000'
+		color:'#9F7E66'
 	},
-	'rust-brown':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'rust-brown': {
+		ttx: '5-100 TTX',
+		herbs: ['Madder (Rubia cordifolia)','wood flower','Iron Earth','Sea Salt'],
+		pantone: '16-1331 TPX',
+		name: 'Rust Brown',
+		color: '#CB9876'
 	},
-	'beet-maroon':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'beet-maroon': {
+		ttx: '8-104 TTX',
+		herbs: ['Madder (Rubia cordifolia)','wood flower','Iron Earth','Sea Salt'],
+		pantone: '19-1245 TPX',
+		name: 'Beet Maroon',
+		color: '#88483D'
 	},
-	'madder-red':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'madder-red': {
+		ttx: '8-100 TTX',
+		herbs: ['Madder (Rubia cordifolia)','Pomegranate (Punica granatum)','Iron Earth'],
+		pantone: '18-1547 TPX',
+		name: 'Madder Red',
+		color: '#923430'
 	},
-	'burnt-grey':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'burnt-grey': {
+		ttx: '9-101 TTX',
+		herbs: ['Iron Earth','Alum (aluminum sulfate)','Pomegranate (Punica granatum)'],
+		pantone: '15-1306 TPX',
+		name: 'Brunt Grey',
+		color: '#B5A994'
 	},
-	'heena-brown':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'heena-brown': {
+		ttx: '6-100 TTX',
+		herbs: ['Iron Earth','Alum (aluminum sulfate)','Pomegranate (Punica granatum)'],
+		pantone: '15-0719 TPX',
+		name: 'Heena Brown',
+		color: '#BCAB7D'
 	},
-	'cutch-brown':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'cutch-brown': {
+		ttx: '3-102 TTX',
+		herbs: ['Madder (Rubia cordifolia)','Pomegranate (Punica granatum)','Iron Earth'],
+		pantone: '18-0724 TPX',
+		name: 'Cutch Brown',
+		color: '#7E7253'
 	},
-	'oak-brown':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'oak-brown': {
+		ttx: '9-102 TTX',
+		herbs: ['Madder (Rubia cordifolia)','Pomegranate (Punica granatum)','Iron Earth'],
+		pantone: '17-0909 TPZ',
+		name: 'Oak Brown',
+		color: '#7F7263'
 	},
-	'woad-blue':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'woad-blue': {
+		ttx: '10-100 TTX',
+		herbs: ['Indigo (Indigofera)'],
+		pantone: '18-4029 TPX',
+		name: 'Woad Blue',
+		color: '#3A5E89'
 	},
-	'chebula-black':{
-		ttx:0,
-		herbs:[],
-		pantone:0,
-		name:'',
-		color:'#000'
+	'chebula-black': {
+		ttx: '9-100 TTX',
+		herbs: ['Harda (Terminalia chebula)','Iron','Dhavid (galls of tamarisk & tamarind(Kachu ka Aata))'],
+		pantone: '19-0000 TPX',
+		name: 'Chebula Black',
+		color: '#383938'
 	}
 }
+
+
+function fillShadeModal(i){
+	var html = '';	
+	html+='<div class="name">'+shadeCardData[i].name+'</div>';
+	html+='<div class="ttx"><strong>Truetone No: </strong>'+shadeCardData[i].ttx+'</div>';
+	html+='<div class="pantone"><strong>Pantone No: </strong>'+shadeCardData[i].pantone+'</div>';
+	html+='<div class="clearfix"></div>';
+	html+='<div class="herbs"><strong>Herbs Used: </strong><br>';
+	shadeCardData[i].herbs.forEach(function(elem){
+		html+='<span>'+elem+'</span>';
+	})
+	html+='</div>';
+	$('#shade-card-modal .modal-body').html(html);
+	$('#shade-card-modal .modal-header').css('background',shadeCardData[i].color);
+}
+
 
 $(document).ready( function() {
 	$('.pmd-tabs').pmdTab();
@@ -198,6 +215,7 @@ $(document).ready( function() {
 	// });
 
 	$('.shade-card').click(function(){
+		fillShadeModal('madder-red');
 		$('#shade-card-modal').modal();
 	});
 
